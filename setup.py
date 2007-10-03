039d64a137e8b9d6d9575c055b3d2122abe5b9fe
Denys Mishunov <denys@jarn.com>
2007-10-03 07:10:51 +0000

    altered description for the package

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='webcouturier.dropdownmenu',
       version=version,
       description="Adds a dropdown functionality to global navigation in Plone",
       long_description="""\
-After installation you will get the dropdown menus for this items in global navigation that have the subitems. Submenus are build based on teh same policy as navigation portlet, so it will show the same tree as you would get in navigation portlet being in appropriate section""",
+You will get the dropdown menus for those items in global navigation that have the subitems. Submenus are build based on the same policy as the Site Map, so it will show the same tree as you would get in the Site Map or navigation portlet being in appropriate section. Requires plone.browserlayer to be installed in your site.""",
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
