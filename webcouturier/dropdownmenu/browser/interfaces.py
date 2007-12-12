82947b3a6dc3ad5d99d70525feac4b9f60275ad5
Denys Mishunov <denys@jarn.com>
2007-12-12 20:51:51 +0000

    removed unnecessary comment for interface

--- webcouturier/dropdownmenu/browser/interfaces.py
+++ webcouturier/dropdownmenu/browser/interfaces.py
@@ -2,11 +2,7 @@ from zope.interface import Interface
 from plone.theme.interfaces import IDefaultPloneLayer
 
 class IDropdownSpecific(IDefaultPloneLayer):
-    """Marker interface that defines a Zope 3 skin layer bound to a Skin
-       Selection in portal_skins.
-       If you need to register a viewlet only for the "Everest Poker Theme"
-       skin, this is the interface that must be used for the layer attribute
-       in everestPokerTheme/browser/configure.zcml.
+    """Marker interface that defines a Zope 3 browser layer.
     """
 
 class IDropdownMenuViewlet(Interface):
