9fa2e9195ac1a9bbfc41144b1e0cce0a71044099
Denys Mishunov <denys@jarn.com>
2008-04-19 20:28:41 +0000

    merged fschulze's changes from 1.1.5 branch

--- webcouturier/dropdownmenu/browser/dropdown.py
+++ webcouturier/dropdownmenu/browser/dropdown.py
@@ -46,48 +46,51 @@ class DropdownMenuViewlet(common.GlobalSectionsViewlet):
         self.properties = getToolByName(self.context, 'portal_properties').navtree_properties
         self.data = Assignment()
 
-    def getTabObject(self, tabUrl=''):
-        # get path for current tab's object
-        tabPath = tabUrl.split(self.portal_url)[-1]
+    def getTabObject(self, tabUrl='', tabPath=None):
+        if tabPath is None:
+            # get path for current tab's object
+            tabPath = tabUrl.split(self.portal_url)[-1]
 
-        if tabPath == '' or '/view' in tabPath:
-            # It's either the 'Home' or Image tab. It can't have any dropdown            
-            return
-            
-        if tabPath.startswith("/"):
-            tabPath = tabPath[1:]
-        elif tabPath.endswith('/'):
-            # we need a real path, without a slash that might appear 
-            # at the end of the path occasionally
-            tabPath = str(tabPath.split('/')[0])
-            
-        if tabPath.split('%20'):
-            # we have the space in object's ID that has to be 
-            # converted to the real spaces
-            tabPath = tabPath.replace('%20', ' ').strip()
-
-        if tabPath != '':
-            tabObj = self.portal.restrictedTraverse(tabPath, None) 
-            
-            if tabObj is None:
-                # just in case we have missed any possible path
-                # in conditions above
+            if tabPath == '' or '/view' in tabPath:
+                # It's either the 'Home' or Image tab. It can't have any dropdown            
                 return
- 
-            strategy = getMultiAdapter((tabObj, self.data), INavtreeStrategy)         
-            # XXX This works around a bug in plone.app.portlets which was
-            # fixed in http://dev.plone.org/svn/plone/changeset/18836
-            # When a release with that fix is made this workaround can be
-            # removed and the plone.app.portlets requirement in setup.py
-            # be updated.
-            if strategy.rootPath is not None and strategy.rootPath.endswith("/"):
-                strategy.rootPath = strategy.rootPath[:-1]
             
-            queryBuilder = DropdownQueryBuilder(tabObj)
-            query = queryBuilder()
-
-            data = buildFolderTree(tabObj, obj=tabObj, query=query, strategy=strategy)
+            if tabPath.startswith("/"):
+                tabPath = tabPath[1:]
+            elif tabPath.endswith('/'):
+                # we need a real path, without a slash that might appear 
+                # at the end of the path occasionally
+                tabPath = str(tabPath.split('/')[0])
             
-            bottomLevel = self.data.bottomLevel or self.properties.getProperty('bottomLevel', 0)
+            if tabPath.split('%20'):
+                # we have the space in object's ID that has to be 
+                # converted to the real spaces
+                tabPath = tabPath.replace('%20', ' ').strip()
 
-            return self.recurse(children=data.get('children', []), level=1, bottomLevel=bottomLevel).strip()
+        if tabPath == '':
+            return
+
+        tabObj = self.portal.restrictedTraverse(tabPath, None)
+
+        if tabObj is None:
+            # just in case we have missed any possible path
+            # in conditions above
+            return
+
+        strategy = getMultiAdapter((tabObj, self.data), INavtreeStrategy)
+        # XXX This works around a bug in plone.app.portlets which was
+        # fixed in http://dev.plone.org/svn/plone/changeset/18836
+        # When a release with that fix is made this workaround can be
+        # removed and the plone.app.portlets requirement in setup.py
+        # be updated.
+        if strategy.rootPath is not None and strategy.rootPath.endswith("/"):
+            strategy.rootPath = strategy.rootPath[:-1]
+
+        queryBuilder = DropdownQueryBuilder(tabObj)
+        query = queryBuilder()
+
+        data = buildFolderTree(tabObj, obj=tabObj, query=query, strategy=strategy)
+
+        bottomLevel = self.data.bottomLevel or self.properties.getProperty('bottomLevel', 0)
+
+        return self.recurse(children=data.get('children', []), level=1, bottomLevel=bottomLevel).strip()
