fb7b31f327129b9d55869e240a5624dea5751375
Denys Mishunov <denys.mishunov@gmail.com>
2011-08-12 23:14:20 +0200

    Significantly simplified JS due to the jQuery usage

--- src/webcouturier/dropdownmenu/browser/dropdown.js
+++ src/webcouturier/dropdownmenu/browser/dropdown.js
@@ -1,19 +1,8 @@
-function sfHover() {
-    var gnav = document.getElementById("portal-globalnav");
-    if (gnav == null) {
-        return;
-    }
-    var sfEls = gnav.getElementsByTagName("LI");
-    // alert("DEBUG");
-    for (var i=0; i<sfEls.length; i++) {
-        sfEls[i].onmouseover=function() {
-            this.className+=" sfhover";
-        }
-        sfEls[i].onmouseout=function() {
-            this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
-        }
-    }
-}
-if (window.attachEvent) {
-    registerPloneFunction(sfHover);
-}
+/* The following line defines global variables defined elsewhere. */
+/*globals $, jQuery*/
+
+jQuery(function ($) {
+    $('#portal-globalnav .noClick').click(function (e) {
+        e.preventDefault();
+    });
+});
