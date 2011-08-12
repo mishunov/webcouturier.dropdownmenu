0581ad6f767da0630742a5e081467f6cc4353574
Denys Mishunov <denys.mishunov@gmail.com>
2011-08-12 22:11:50 +0200

    Tabs to spaces

--- src/webcouturier/dropdownmenu/browser/dropdown.js
+++ src/webcouturier/dropdownmenu/browser/dropdown.js
@@ -3,16 +3,16 @@ function sfHover() {
     if (gnav == null) {
         return;
     }
-	var sfEls = gnav.getElementsByTagName("LI");
+    var sfEls = gnav.getElementsByTagName("LI");
     // alert("DEBUG");
-	for (var i=0; i<sfEls.length; i++) {
-		sfEls[i].onmouseover=function() {
-			this.className+=" sfhover";
-		}
-		sfEls[i].onmouseout=function() {
-			this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
-		}
-	}
+    for (var i=0; i<sfEls.length; i++) {
+        sfEls[i].onmouseover=function() {
+            this.className+=" sfhover";
+        }
+        sfEls[i].onmouseout=function() {
+            this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
+        }
+    }
 }
 if (window.attachEvent) {
     registerPloneFunction(sfHover);
